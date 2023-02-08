6ca6700e7fe5e549972142a92584832d9047f685
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-02-07 21:35:14 -0300

    added 404 route

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,10 @@ app.get('/', (req: Request, res: Response,) => {
 
 app.use('/api', router)
 
+app.use('*', (req, res) => {
+    res.status(404).send({ error:"this route does not exist"})
+})
+
 app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', { root: __dirname });
     res.status(404).send('404')
